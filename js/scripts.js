0a7f2eb552caea63a9f0648568c5b1627e45b2d5
Jessiecorgy <jessiecorgy@gmail.com>
2014-11-20 02:14:31 -0800

    styling

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,14 @@ $('#rumble-element').trigger('startRumble');
 
 // Stop rumble on element
 $('#rumble-element').trigger('stopRumble');
+
+
+$('#rumble-element').jrumble({
+	speed: 100
+});
+
+$('rumble-element').hover(function(){
+	$(this).trigger('startRumble');
+}, function(){
+	$(this).trigger('stopRumble');
+});
