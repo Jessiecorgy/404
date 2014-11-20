ed98b0652c424aae591dc028b4e57f3b349deb78
Jessiecorgy <jessiecorgy@gmail.com>
2014-11-20 01:11:13 -0800

    yada
    yada

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Initialize jRumble on Selector
+$('#rumble-element').jrumble();
+
+// Start rumble on element
+$('#rumble-element').trigger('startRumble');
+
+// Stop rumble on element
+$('#rumble-element').trigger('stopRumble');
